7703acc7bd7bc6bd65ae4572836a8cea895ecdfa
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-05-26 12:48:13 -0400

    first commit

---
+++ main.dart
@@ -0,0 +1,10 @@
+main() {
+  int empreado = 10;
+  double pi = 3.1415;
+  print('el numero de empleado es $empreado y pi vale: $pi');
+  String nombre = 'kervis';
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length -1]);
+
+}
